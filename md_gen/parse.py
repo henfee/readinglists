93541160b55203bc88f5e8f2f86dd6496f8d201c
Eric Xu <exu@fitbit.com>
2015-02-11 21:37:37 -0800

    changes to generate markdown

--- md_gen/parse.py
+++ md_gen/parse.py
@@ -1,14 +1,17 @@
+import re
+import string
 import sys
 sys.path.append('/Users/exu/PlayGround/readinglists/')
 
 from key.keys import *
 from amazon.api import AmazonAPI
+from html2text import html2text
 
-
-import re
 pattern = re.compile("https?://.*amazon.com/gp/product/([0-9]+)/.*")
-
 amazon = AmazonAPI(AMAZON_ACCESS_KEY_ID, AMAZON_SECRET_ACCESS_KEY, AMAZON_ASSOC_TAG, MaxQPS=0.9)
+
+def uprint(s):
+    print s.encode('utf-8')
 
 def get_asin(url):
     global pattern
@@ -18,13 +21,49 @@ def get_asin(url):
 
 def read_file():
     if (len(sys.argv) < 1):
-        print "need to supply a file name"
+        print "Please provide a file that includes a list of Amazon links."
         sys.exit(-1)
     fname = sys.argv[1]
     f = open(fname, 'r')
-    for l in f.readlines()[:4]:
+    products = []
+    for l in f.readlines():
         product = amazon.lookup(ItemId=get_asin(l))
-        print product.title
+        products.append([product.title, product.editorial_review, product.large_image_url, product.offer_url])
+        print "Got product", product.title
+    return products
+
+rtitle = re.compile('(.*)(\(.*\))')
+def normalize_title(title):
+    """ Book titles are long. We crop out the last part that is in (part)"""
+    splits =  re.findall(rtitle, title)
+    if splits:
+        new_title = splits[0][0]
+    else:
+        new_title = title
+    return new_title
+
+def sanitize_text(t):
+    s = html2text(t)
+    s = string.replace(s, "'", "&rsquo;")
+    s = string.replace(s, "**", "*")
+    return s
 
 if __name__ == '__main__':
-    read_file()
+    import os.path
+    import cPickle
+    pickle_file = 'products.pickle'
+    products = None
+    if os.path.isfile(pickle_file):
+        products = cPickle.load(open(pickle_file, 'r'))
+    else:
+        products = read_file()    
+        f = open(pickle_file, "wb")
+        cPickle.dump(products, f)
+    for product in products:
+        title = normalize_title(product[0])
+        uprint(title)
+        print '=' * len(title)
+        review = sanitize_text(product[1])
+        uprint(review)
+        print
+
